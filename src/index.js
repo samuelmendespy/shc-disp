3ae711149761a2b93e8c01b12e9ed0365e0887a8
pysmendes <75790706+pysmendes@users.noreply.github.com>
2022-05-08 22:29:27 -0300

    Atualizar arquivos

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import * as serviceWorker from './serviceWorker'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -10,3 +11,4 @@ root.render(
   </React.StrictMode>
 );
 
+serviceWorker.register();
